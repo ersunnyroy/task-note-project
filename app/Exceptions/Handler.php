5db668d4e0ae75291b3840ea9971bb5bdc28231d
Suni <suni.kumar@qualibar.com>
2024-02-28 06:59:15 +0530

    Handle api invalid tokens

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,10 @@
 namespace App\Exceptions;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -39,14 +43,35 @@ class Handler extends ExceptionHandler
         });
     }
 
-    protected function unauthenticated($request, AuthenticationException $exception)
+     public function render($request, Throwable $exception)
     {
-        // Return a simple JSON response for unauthorized requests
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
+        if ($exception instanceof AuthenticationException) {
+            return $this->handleAuthenticationException($exception, $request);
+        } elseif (
+            $exception instanceof TokenExpiredException ||
+            $exception instanceof TokenInvalidException ||
+            $exception instanceof JWTException
+        ) {
+            return $this->handleTokenException($exception);
+        } elseif ($exception instanceof HttpException && $exception->getStatusCode() === 401) {
+            return $this->handleHttpException($exception);
         }
 
-        // For non-JSON requests, you can redirect or customize the response as needed
-        return redirect()->guest(route('login'));
+        return parent::render($request, $exception);
+    }
+
+    protected function handleAuthenticationException(AuthenticationException $exception, $request)
+    {
+        return response()->json(['error' => 'Unauthenticated'], 401);
+    }
+
+    protected function handleTokenException($exception)
+    {
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    protected function handleHttpException(HttpException $exception)
+    {
+        return response()->json(['error' => 'Unauthorized'], 401);
     }
 }
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,6 +3,8 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Auth\AuthenticationException;
+
 
 class Authenticate extends Middleware
 {
